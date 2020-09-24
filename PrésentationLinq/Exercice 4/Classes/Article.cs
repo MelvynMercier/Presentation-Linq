fd2a428f066124e089cde2273c8d41d6bd52e962
MelvynMercier <43468212+MelvynMercier@users.noreply.github.com>
2020-09-24 15:46:00 +0200

    Exercice 4

---
+++ PrésentationLinq/Exercice 4/Classes/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice_4.Classes
+{
+    public class Article
+    {
+
+        public string Nom { get; set; }
+        public DateTime Date { get; set; }
+
+        public Article(string nom, DateTime date)
+        {
+            Nom = nom;
+            Date = date;
+        }
+    }
+}
---
+++ PrésentationLinq/Exercice 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using Exercice_4.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listArticle = new List<Article>
+            {
+                new Article("Article 1", new DateTime(2016, 4, 21)),
+                new Article("Article 2", new DateTime(2002, 11, 6)),
+                new Article("Article 3", new DateTime(2408, 1, 30)),
+                new Article("Article 4", new DateTime(2130, 7, 10))
+            };
+
+            var articles = (from article in listArticle
+                          where article.Date <= new DateTime(2020, 9, 24)
+                            select article).ToList();
+
+            articles.ForEach(art =>
+            {
+                Console.WriteLine($"Nom : {art.Nom}, Date : {art.Date}");
+            });
+        }
+    }
+}
